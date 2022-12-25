f805547c60f747c8324d40c4e65b9d4a19c287d0
Khripunov_P <dante-pash@mail.ru>
2022-12-26 02:10:32 +0300

    add PrintArray

--- Program.cs
+++ Program.cs
@@ -2,3 +2,22 @@
 // длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами.
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < (array.Length - 1))
+            Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine($"]");
+}
+
+Console.WriteLine("Введите элементы массива через запятую");
+string[] str = Console.ReadLine().Split(',');
+
+Console.WriteLine("Начальный массив");
+PrintArray(str);
+
